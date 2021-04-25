e6ebefef3ac796fcbdf17abff4de16fa7f3c6a14
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-04-25 14:42:23 +0100

    added a stored procedure for getting user details

---
+++ DatBaseManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using DatBaseManager.Library.Internal.DataAccess;
+using DatBaseManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatBaseManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "MyDataBase");
+
+            return output;
+        }
+    }
+}
---
+++ DataBaseManager/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using DatBaseManager.Library.DataAccess;
+using DatBaseManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DataBaseManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public UserModel GetById()
+        {
+            string UserId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserById(UserId).First();
+        }
+
+    }
+}
